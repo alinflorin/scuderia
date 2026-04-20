53264b278cadea65d805ad36e354ed92f29f7a8d
Alin-Florin Ciu <alinflorinciu@icloud.com>
2026-04-20 16:18:15 +0300

    asd

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Fix ownership of mounted volumes — must run as root before dropping to appuser
-chown -R appuser:appuser /app/persist
+chown -R appuser:appuser /app
 chown -R appuser:appuser /home/appuser
 
 exec gosu appuser "$@"
